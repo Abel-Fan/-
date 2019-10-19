759b72e11ffc52f10e5abcf37d59bab50dbd9fd6
Abel_fan <842615663@qq.com>
2019-10-19 11:04:06 +0800

    后台02

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
+import pymysql
 # 实例化web服务器
 app = Flask(__name__)
-
 # 创建首页的路由
 @app.route("/")
 def index():
@@ -13,6 +12,17 @@ def index():
 def admin():
     return render_template("admin.html")
 
+@app.route("/addgoods",methods=['POST'])
+def addgoods():
+    title = request.form['title']
+    title2 = request.form['title2']
+    info = request.form['info']
+    price = request.form['price']
+    file = request.files['img']
+    filename = "static/upload/"+file.filename
+    file.save(filename)
+    print(title,title2,info,price)
+    return "1"
 
 
 if __name__ == "__main__":
